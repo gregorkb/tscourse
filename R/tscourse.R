af9cde848421619e1bae2935b1232fa854306730
Karl Gregory <gregorkb@stat.sc.edu>
2019-02-07 07:58:16 -0500

    had to fix innov alg to accomate n=1

--- R/tscourse.R
+++ R/tscourse.R
@@ -105,45 +105,50 @@ innov.hstep<- function(X,h,K){
 	v[2] <- K[2,2] - Theta[1,1]^2*v[1]
 	X.pred[2] <- Theta[1,1]*X[1]
 	
-	for(k in 2:n)
+	if(n > 1)
 	{
 		
-		Theta[k,k] <- K[k+1,1] / v[1]
-						
-		for(j in 1:(k-1))
-		{
-			
-			Theta[k,k-j] <- (K[k+1,j+1]-sum(Theta[j,j:1]*Theta[k,k:(k-j+1)]*v[1:j]))/v[j+1]
-				
-		}
-		
-		v[k+1] <- K[k+1,k+1] - sum( Theta[k,k:1]^2 * v[1:k] )
-		X.pred[k+1] <- sum( Theta[k,1:k] *(X[k:1] - X.pred[k:1]) )
-		
-	}
-
-	if(h > 1)
-	{
-	
-		for(k in (n+1):(n+h-1))
+		for(k in 2:n)
 		{
 			
 			Theta[k,k] <- K[k+1,1] / v[1]
-			
+							
 			for(j in 1:(k-1))
 			{
 				
-				Theta[k,k-j]<-(K[k+1,j+1]-sum(Theta[j,j:1]*Theta[k,k:(k-j+1)]*v[1:j]))/v[j+1]
+				Theta[k,k-j] <- (K[k+1,j+1]-sum(Theta[j,j:1]*Theta[k,k:(k-j+1)]*v[1:j]))/v[j+1]
 					
 			}
-		
-			v[k+1] <- K[k+1,k+1] - sum( Theta[k,(k-n+1):k]^2 * v[n:1] )
-			X.pred[k+1] <- sum( Theta[k,(k-n+1):k] *(X[n:1] - X.pred[n:1]) )				
 			
+			v[k+1] <- K[k+1,k+1] - sum( Theta[k,k:1]^2 * v[1:k] )
+			X.pred[k+1] <- sum( Theta[k,1:k] *(X[k:1] - X.pred[k:1]) )
+			
+		}
+	
+		if(h > 1)
+		{
+		
+			for(k in (n+1):(n+h-1))
+			{
+				
+				Theta[k,k] <- K[k+1,1] / v[1]
+				
+				for(j in 1:(k-1))
+				{
+					
+					Theta[k,k-j]<-(K[k+1,j+1]-sum(Theta[j,j:1]*Theta[k,k:(k-j+1)]*v[1:j]))/v[j+1]
+						
+				}
+			
+				v[k+1] <- K[k+1,k+1] - sum( Theta[k,(k-n+1):k]^2 * v[n:1] )
+				X.pred[k+1] <- sum( Theta[k,(k-n+1):k] *(X[n:1] - X.pred[n:1]) )				
+				
+			}
+	
 		}
 
 	}
-	
+		
 	output <- list( X.pred = X.pred,
 					v = v)
 					
